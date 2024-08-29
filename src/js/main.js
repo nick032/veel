63ca402003e1c4a0d7848b0dba12e489a98272c3
nick <nick.x.a.k@gmail.com>
2024-08-29 17:06:47 +0300

    Stage 3, Third section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-import './_components.js';
+import "./_components.js";
+
+//Карта
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("origin__map", {
+    center: [55.708511, 37.455382],
+    controls: [],
+    zoom: 15,
+  });
+}
