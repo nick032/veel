cc56dea80f688220e50a3b1234e64c621c9b8c8f
nick <nick.x.a.k@gmail.com>
2024-09-07 09:59:48 +0300

    Stage 9, map placemarker

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,40 @@
 import "./_components.js";
-import "./components/hystmodal.min.js"
-import "./components/jquery.fancybox.min.js"
+import "./components/hystmodal.min.js";
+import "./components/jquery.fancybox.min.js";
 
 //Карта
 ymaps.ready(init);
 function init() {
   var myMap = new ymaps.Map("origin__map", {
-    center: [55.708511, 37.455382],
-    controls: [],
-    zoom: 15,
-  });
+      center: [55.708511, 37.455382],
+      controls: [],
+      zoom: 14,
+    }),
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+    myPlacemark = new ymaps.Placemark(
+      myMap.getCenter(),
+      {
+        hintContent: "Собственный значок метки",
+      },
+      {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: "default#image",
+        // Своё изображение иконки метки.
+        iconImageHref: "./img/map-markers/metro.png",
+        // Размеры метки.
+        // iconImageSize: [30, 42],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38],
+      }
+    );
+
+    myMap.geoObjects
+      .add(myPlacemark);
 }
 
 // Модальные окна
@@ -19,40 +44,36 @@ const modalForm = new HystModal({
 
 const modalSuccess = new HystModal({
   linkAttributeName: "data-hystmodal",
-})
+});
 
 // burger menu
 function burgerMenu(selector) {
-
   let menu = $(selector);
 
-  let button = menu.find('.nav__burger-menu', '.burger');
+  let button = menu.find(".nav__burger-menu", ".burger");
 
-  let links = menu.find('.nav__link');
-  let menuList = $('.nav__list')
+  let links = menu.find(".nav__link");
+  let menuList = $(".nav__list");
 
   // let overlay = menu.find('.burger-menu_overlay');
 
-  button.on('click', (e) => {
+  button.on("click", (e) => {
     e.preventDefault();
     toggleMenu();
   });
 
-  links.on('click', () => toggleMenu());
+  links.on("click", () => toggleMenu());
   // overlay.on('click', () => toggleMenu());
 
-  function toggleMenu(){
-    menu.toggleClass('burger-menu__active');
+  function toggleMenu() {
+    menu.toggleClass("burger-menu__active");
 
-    if (menu.hasClass('burger-menu__active')) {
-      $('body').css('overlow', 'hidden');
+    if (menu.hasClass("burger-menu__active")) {
+      $("body").css("overlow", "hidden");
     } else {
-      $('body').css('overlow', 'visible');
+      $("body").css("overlow", "visible");
     }
   }
 }
 
-burgerMenu('.nav');
-
-
-
+burgerMenu(".nav");
